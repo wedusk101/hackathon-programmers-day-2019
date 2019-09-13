53e5e94b3976ee16b847d0be43c6588463654d77
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2019-09-14 04:08:10 +0530

    Update

--- Programmers_day_2019/main.cpp
+++ Programmers_day_2019/main.cpp
@@ -1,55 +1,25 @@
 #include <iostream>
 #include <Windows.h>
 #include <mmsystem.h>
-const int width = 240, height = 180, bufferSize = height * width; wchar_t *frameBuffer = new wchar_t[bufferSize]; DWORD bytesWritten = 0;
+const int width = 120, height = 40, bufferSize = height * width; wchar_t *frameBuffer = new wchar_t[bufferSize]; DWORD bytesWritten = 0;
 HANDLE hAppWnd = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 struct Vec2 {
-	double x; double y;	Vec2() : x(0), y(0) {} 
+	double x, y;	Vec2() : x(0), y(0) {}
 	Vec2(const double &x_, const double &y_) : x(x_), y(y_) {}
-	void draw() { WriteConsoleOutputCharacter(hAppWnd, (LPCTSTR)frameBuffer, bufferSize, { (short)x, (short)y }, &bytesWritten); }
+	void draw(char ch) { frameBuffer[(int)y * height + (int)x] = ch; WriteConsoleOutputCharacter(hAppWnd, (LPCTSTR)frameBuffer, bufferSize, { 0, 0 }, &bytesWritten); }
 };
-struct Circle {
-	Vec2 center; double radius;
-	Circle(const Vec2 &c_, const double &r_) : center(c_), radius(r_) {}
-	void draw()	{
-
+void clearFrameBuffer() { for (int i = 0; i < bufferSize; i++) frameBuffer[i] = ' '; frameBuffer[bufferSize - 1] = '\0'; WriteConsoleOutputCharacter(hAppWnd, (LPCTSTR)frameBuffer, bufferSize, { 0, 0 }, &bytesWritten); }
+struct Paddle {
+	Vec2 top, bottom, middle;
+	Paddle(const Vec2 &top_) : top(top_) { middle = Vec2(top.x, top.y); bottom = Vec2(top.x , top.y);}
+	void draw() {int ch = 0xB2; top.draw(ch); middle.draw(ch); bottom.draw(ch);}
+};
+int main() {
+	SetConsoleActiveScreenBuffer(hAppWnd); clearFrameBuffer();
+	Vec2 src = Vec2(0, 0); Vec2 dst = Vec2(0, 40); Paddle leftPaddle(Vec2(0, 19));
+	while (true) {
+		src.draw('*'), dst.draw('?');  leftPaddle.draw();
 	}
-};
-struct Line {
-	Vec2 src; Vec2 dst;
-	Line(const Vec2 &src_, const Vec2&dst_) : src(src_), dst(dst_) {}
-	void draw() { // DDA line drawing algorithm
-		double slope = 0.0, x = 0.0, y = 0.0;
-		int dy = dst.y - src.y, dx = dst.x - src.x, sgn = 0;
-		if (dx == 0)
-			for (y = src.y; y <= dst.y; ++y) { Vec2 p(x, y); p.draw(); }
-		else if (dy == 0)
-			for (x = src.x; x <= dst.x; ++x) { Vec2 p(x, y); p.draw(); }
-		else {
-			if (abs(dy) <= abs(dx)) { // gentle slope
-				if (src.x > dst.x) // swaps the srcing point if needed
-					swap(src, dst);
-				src.draw();	slope = dy / (double)dx; sgn = sign(slope);
-				for (x = src.x, y = src.y; x <= dst.x; x += sgn) {y += slope;	Vec2 p(x, y); p.draw();}}
-			else { // steep slope
-				if (src.y > dst.y) // swaps the srcing point if needed
-					swap(src, dst);
-				dst.draw(); slope = dx / (double)dy; sgn = sign(slope);
-				for (x = src.x, y = src.y; y <= dst.y; y += sgn) { x += slope; Vec2 p(x, y); p.draw();}}}}
-};
-struct Rectangle {
-	Vec2 topLeft; Vec2 bottomRight;
-	Rectangle(const Vec2 &tl_, const Vec2&br_) : topLeft(tl_), bottomRight(br_) {}
-	void draw() {
-		Vec2 bottomLeft = Vec2(topLeft.x, bottomRight.y); Vec2 topRight = Vec2(topLeft.y, bottomRight.x);
-		Line top = Line(topLeft, topRight); Line bottom = Line(bottomLeft, bottomRight); Line left = Line(topLeft, bottomLeft); Line right = Line(topRight, bottomRight);
-		top.draw(); bottom.draw(); left.draw(); right.draw();}
-};
-void swap(Vec2 &a, Vec2 &b){Vec2 tmp = a; a = b; b = tmp;}
-int sign(const double &val) { return (val == 0) ? 0 : (val < 0) ? -1 : 1;}
-int main() {	
-	SetConsoleActiveScreenBuffer(hAppWnd);
-	frameBuffer[bufferSize - 1] = '\0';	
-
-	delete [] frameBuffer;
+	for(int i = 0; i < 360;)
+	delete[] frameBuffer;
 }
